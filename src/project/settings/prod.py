436cfa58b1ebfc2c68d76099fd3e9c8006cc9dc1
Jacob <jacob.maslanka@protonmail.com>
2018-05-21 21:19:47 +0200

    simple nginx for docker, s3 settings

--- src/project/settings/prod.py
+++ src/project/settings/prod.py
@@ -8,13 +8,23 @@ SESSION_SAVE_EVERY_REQUEST = True
 CSRF_COOKIE_SECURE = True
 CSRF_COOKIE_HTTPONLY = True
 
-if env('SENTRY_URL', default=''):
-    INSTALLED_APPS += [
-        'raven.contrib.django.raven_compat',
-    ]
-    RAVEN_CONFIG = {
-        'dsn': env('SENTRY_URL', default=''),
-    }
+AWS_ACCESS_KEY_ID = env('AWS_STATIC_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = env('AWS_STATIC_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = env('AWS_STORAGE_BUCKET_NAME')
+AWS_QUERYSTRING_AUTH = env.bool('AWS_QUERYSTRING_AUTH')
+AWS_S3_ENDPOINT_URL = env('AWS_S3_ENDPOINT_URL')
+
+INSTALLED_APPS += [
+    'raven.contrib.django.raven_compat',
+    'storages',
+]
+
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+RAVEN_CONFIG = {
+    'dsn': env('SENTRY_URL'),
+}
 
 LOGGING = {
     'version': 1,
