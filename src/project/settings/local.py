f773d36b97f7bc1fabbf0d41c5583ed0175d8dbd
Jacob <jacob.maslanka@protonmail.com>
2018-05-21 20:42:16 +0200

    alpine -> slime, added logging + sentry

--- src/project/settings/local.py
+++ src/project/settings/local.py
@@ -1,6 +1,5 @@
 from .default import *  # noqa
 
-
 AUTH_PASSWORD_VALIDATORS = list()
 TEMPLATES[0]['OPTIONS']['context_processors'] += [
     'django.template.context_processors.debug',
--- src/project/settings/prod.py
+++ src/project/settings/prod.py
@@ -7,3 +7,55 @@ SESSION_SAVE_EVERY_REQUEST = True
 
 CSRF_COOKIE_SECURE = True
 CSRF_COOKIE_HTTPONLY = True
+
+if env('SENTRY_URL', default=''):
+    INSTALLED_APPS += [
+        'raven.contrib.django.raven_compat',
+    ]
+    RAVEN_CONFIG = {
+        'dsn': env('SENTRY_URL', default=''),
+    }
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': True,
+    'root': {
+        'level': 'WARNING',
+        'handlers': ['sentry'],
+    },
+    'formatters': {
+        'verbose': {
+            'format': '%(levelname)s %(asctime)s %(module)s '
+                      '%(process)d %(thread)d %(message)s'
+        },
+    },
+    'handlers': {
+        'sentry': {
+            'level': 'WARNING',
+            'class': 'raven.contrib.django.raven_compat.handlers.SentryHandler',
+            'tags': {'custom-tag': 'x'},
+        },
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose'
+        }
+    },
+    'loggers': {
+        'django.db.backends': {
+            'level': 'ERROR',
+            'handlers': ['console'],
+            'propagate': False,
+        },
+        'raven': {
+            'level': 'DEBUG',
+            'handlers': ['console'],
+            'propagate': False,
+        },
+        'sentry.errors': {
+            'level': 'DEBUG',
+            'handlers': ['console'],
+            'propagate': False,
+        },
+    },
+}
